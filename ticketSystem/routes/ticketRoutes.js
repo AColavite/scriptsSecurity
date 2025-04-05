7e5b690d3e1cf5f6e060d334798ab13e65cb82ba
AColavite <143002841+AColavite@users.noreply.github.com>
2025-04-05 01:56:28 -0300

    Delete ticketSystem directory

--- ticketSystem/routes/ticketRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createTicket, getMyTickets } = require('../controllers/ticketController');
-const protect = require('../middlewares/auth');
-
-router.post('/', protect, createTicket);
-router.get('/', protect, getMyTickets);
-
-module.exports = router;
